37626dcaf36eae0df1605601eb5391760f632d68
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-07 10:57:44 +0300

    add npm, pnpm, bun, go, cargo packages and rustup toolchans update to the`upgrade-system.sh`

--- Scripts/upgrade-system.sh
+++ Scripts/upgrade-system.sh
@@ -5,14 +5,37 @@ paru_error() {
   echo '$ sudo pacman -Sy --needed archlinux-keyring && pacman -Su'
   exit 1
 }
-
-flatpak_error() {
-  echo $'\nFailed to upgrade flatpack packages'
-  exit 1
-}
+flatpak_error() { echo $'\nFailed to upgrade flatpack packages'; exit 1; }
+npm_error() { echo $'\nFailed to update npm packages'; exit 1; }
+pnpm_error() { echo $'\nFailed to update pnpm packages'; exit 1; }
+bun_error() { echo $'\nFailed to update bun packages'; exit 1; }
+go_error() { echo $'\nFailed to update go binaries'; exit 1; }
+rust_error() { echo $'\nFailed to update rust'; exit 1; }
+cargo_error() { echo $'\nFailed to update cargo binaries'; exit 1; }
 
 echo $'Upgrading system packages:\n$ paru -Syu\n'
 paru -Syu || paru_error
 
 echo $'\nUpgrading flatpack packages:\n$ flatpak update\n'
 flatpak update || flatpak_error
+
+echo $'\nUpdating npm packages:\n$ npm update -g\n'
+npm update -g || npm_error
+
+echo $'\nUpdating pnpm packages:\n$ pnpm update -g\n'
+pnpm update -g || pnpm_error
+
+echo $'\nUpdating bun packages:\n$ bun update -g\n'
+bun update -g || bun_error
+
+echo $'\nUpdating go binaries:\n$ gup update\n'
+gup update || go_error
+
+echo $'\nUpdating rust:\n$ rustup update\n'
+rustup update || rust_error
+
+echo $'\nUpdating cargo binaries:'
+echo $'$ cargo_packages="$(jq -r \'.installs | keys[] | split(" ")[0]\' < "$CARGO_HOME/.crates2.json")"'
+echo $'cargo install --locked "$cargo_packages"r'
+cargo_packages="$(jq -r '.installs | keys[] | split(" ")[0]' < "$CARGO_HOME/.crates2.json")" || cargo_error
+cargo install --locked "$cargo_packages" || cargo_error
