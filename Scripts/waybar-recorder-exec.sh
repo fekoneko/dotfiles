1d73e7661301c93a9ed7cc72b98b34b414de7d58
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-24 00:21:19 +0300

    add anki word button to waybar and adjust sizes and paddings

--- Scripts/waybar-recorder-exec.sh
+++ Scripts/waybar-recorder-exec.sh
@@ -3,6 +3,6 @@
 sleep 0.5
 wf_recorder_pid=$(pgrep wf-recorder)
 if [ -n "$wf_recorder_pid" ]
-	then echo '{"text":"      ","tooltip":"Stop recording"}'
-	else echo '{"text":"  󰻂    ","tooltip":"Recording the screen"}'
+	then echo '{"text":"     ","tooltip":"Stop recording"}'
+	else echo '{"text":" 󰻂    ","tooltip":"Recording the screen"}'
 fi
