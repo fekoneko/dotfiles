625f3813683e687fda6e9e06f15427cb1c919520
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-26 23:55:13 +0300

    `sync-archive` script

---
+++ Scripts/sanitize-exfat.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <target directory>"
+  exit 1
+fi
+
+sanitize() {
+  shopt -s extglob;
+
+  filename=$(basename "$1")
+  directory=$(dirname "$1")
+
+  filename_clean=$(echo "$filename" | sed -e 's/[\\/:\*\?"<>\|\x01-\x1F\x7F]//g' -e 's/^\(nul\|prn\|con\|lpt[0-9]\|com[0-9]\|aux\)\(\.\|$\)//i' -e 's/^\.*$//' -e 's/^$/NONAME/')
+
+  if [ "$filename" != "$filename_clean" ]; then
+    mv -v "$1" "$directory/$filename_clean"
+  fi
+}
+
+export -f sanitize
+
+sanitize_dir() {
+  find "$1" -depth -exec bash -c 'sanitize "$0"' {} \;
+}
+
+sanitize_dir "$1"
