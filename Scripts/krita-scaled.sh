064c6a90a8dd061436fa064852e846303437b672
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-30 21:02:05 +0300

    adjust krita scaling for new `xwayland-satellite` version

--- Scripts/krita-scaled.sh
+++ Scripts/krita-scaled.sh
@@ -3,4 +3,4 @@
 # Workaround for Krita scaling on my HiDPI monitor in XWayland
 # This should be removed when Krita supports Wayland
 
-QT_SCALE_FACTOR="1.35" gtk-launch org.kde.krita
+QT_SCALE_FACTOR=0.675 gtk-launch org.kde.krita
