1daffe14bedd0bb332db223b92834e46a172accd
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-30 11:28:33 +0300

    select output for recorder in waybar

--- Scripts/toggle-recording.sh
+++ Scripts/toggle-recording.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 if [ -z $(pgrep wf-recorder) ]; then
-	filename=$(date +%F_%T.mp4)
-	if [ "$1" = "--slurp" ]
-		then wf-recorder -f "$HOME/Videos/$filename" -g "$(slurp -c "#FFFFFF")" >/dev/null 2>&1 &
-		else wf-recorder -f "$HOME/Videos/$filename" >/dev/null 2>&1 &
+	options="$(niri msg outputs | sed -n 's/^Output "\(.*\)" (\(.*\))$/\2     (\1)/p')" || exit 1
+	if [[ $(printf '%s\n' "$options" | wc -l) -gt 1 ]]
+		then selected_option="$(printf '%s' "$options" | wofi --show dmenu)" || exit 1
+		else selected_option="$options"
 	fi
+
+	if [[ -z "$selected_option" ]]; then exit 1; fi
+	output="$(printf '%s' "$selected_option" | sed -n 's/     (.*)$//p')"
+
+	filename=$(date +%F_%T.mp4)
+	wf-recorder -f "$HOME/Videos/$filename" -o "$output" >/dev/null 2>&1 &
 else
 	killall -s SIGINT wf-recorder || exit 1
-	notify-send "Recording stopped"
 	while [ -n "$(pgrep -x wf-recorder)" ]; do wait; done
+	notify-send "Recording stopped"
 fi
