7f9912c1e9e4ef994b29aa0f06ef16ea2599e512
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-07-02 13:08:19 +0300

    i love fractional scaling in x

--- Scripts/krita-scaled.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Super + Shift + K
-# Workaround for Krita scaling on my HiDPI monitor in XWayland
-# This should be removed when Krita supports Wayland
-
-QT_SCALE_FACTOR=0.675 gtk-launch org.kde.krita
