21f8f12be16676412238a8cf57b71bdb20051cea
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-08-19 19:28:09 +0300

    change waybar design

--- Scripts/waybar-recorder-exec.sh
+++ Scripts/waybar-recorder-exec.sh
@@ -2,6 +2,6 @@
 
 wf_recorder_pid="$(pgrep wf-recorder)"
 if [ -n "$wf_recorder_pid" ]
-	then echo '{"text":"     ","tooltip":"Stop recording"}'
-	else echo '{"text":" 󰻂    ","tooltip":"Record the screen"}'
+	then echo '{"text":"","tooltip":"Stop recording"}'
+	else echo '{"text":"󰻂","tooltip":"Record the screen"}'
 fi
