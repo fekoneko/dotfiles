09dce25dc7a8af1fd9ff49f9941591895ce7177c
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-09-01 17:17:18 +0300

    optimize waybar vpn module

--- Scripts/toggle-recording.sh
+++ Scripts/toggle-recording.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "$(pgrep wf-recorder)" ]; then
+start_recording() {
 	options="$(niri msg outputs | sed -n 's/^Output "\(.*\)" (\(.*\))$/\2     (\1)/p')" || exit 1
 	if [ "$(printf '%s\n' "$options" | wc -l)" -gt 1 ]
 		then selected_option="$(printf '%s' "$options" | wofi --show dmenu)" || exit 1
@@ -10,10 +10,17 @@ if [ -z "$(pgrep wf-recorder)" ]; then
 	if [ -z "$selected_option" ]; then exit 1; fi
 	output="$(printf '%s' "$selected_option" | sed -n 's/     (.*)$//p')"
 
-	filename=$(date +%F_%T.mp4)
+	filename=$(date '+%F_%T.mp4')
 	wf-recorder -f "$HOME/Videos/$filename" -o "$output" >/dev/null 2>&1 &
-else
+}
+
+stop_recording() {
 	killall -s SIGINT wf-recorder || exit 1
 	while [ -n "$(pgrep -x wf-recorder)" ]; do wait; done
-	notify-send "Recording stopped"
+	notify-send 'Recording stopped'
+}
+
+if [ -z "$(pgrep wf-recorder)" ]
+	then start_recording
+	else stop_recording
 fi
--- Scripts/waybar-vpn-exec.sh
+++ Scripts/waybar-vpn-exec.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
-sleep 0.5
+# This NetworkManager dispatcher needs to be added to update widget status
+# /etc/NetworkManager/dispatcher.d/vpn-waybar.sh
+#
+#   #!/bin/sh
+#   action="$2"
+#   if [ "$action" = 'down' ] || [ "$action" = 'up' ]; then
+#     killall -USR1 waybar-vpn-exec.sh
+#   fi
 
-name="$(\
-  nmcli connection show --active | \
-  sed -nr 's/^(.*) +[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12} +wireguard.*$/\1/p' | \
-  xargs\
-)"
+update() {
+  name="$(\
+    nmcli connection show --active | \
+    sed -nr 's/^(.*) +[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12} +wireguard.*$/\1/p' | \
+    xargs\
+  )"
 
-if [ -n "$name" ]
-  then echo "{\"tooltip\":\"$name\",\"text\":\"\"}"
-  else echo "{\"tooltip\":\"Wireguard disconnected\",\"text\":\"\"}"
-fi
+  if [ -n "$name" ]
+    then echo "{\"tooltip\":\"$name\",\"text\":\"\"}"
+    else echo "{\"tooltip\":\"Wireguard disconnected\",\"text\":\"\"}"
+  fi
+}
+
+update
+trap update USR1
+
+# Wait in background, not consuming CPU
+while :; do sleep infinity & wait $!; done
