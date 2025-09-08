2b941225baedee50082b73ef5247c57107cc83d7
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-09-08 15:00:53 +0300

    remove unnesessary spaces in japanese output for ocr

--- Scripts/ocr.sh
+++ Scripts/ocr.sh
@@ -7,8 +7,12 @@ cd "$HOME/.cache" || exit 1
 area="$(slurp -b 00000060 -c ffffff)" || exit 1
 grim -g "$area" screen-ocr.png || exit 1
 
-tesseract screen-ocr.png screen-ocr -l "${language:-eng}"; status="$?"
-wl-copy < screen-ocr.txt
+tesseract screen-ocr.png screen-ocr -l "${language:-eng}"; status=$?
+
+if [[ $language == "jpn" ]] || [[ $language == "jpn_vert" ]]
+  then sed 's/ //g' screen-ocr.txt | wl-copy
+  else wl-copy < screen-ocr.txt
+fi
 
 rm screen-ocr.png screen-ocr.txt
-exit "$status"
+exit $status
