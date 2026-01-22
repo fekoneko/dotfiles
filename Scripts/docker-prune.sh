0281080b692c81318a83dafc769f7705bf885b9b
fekoneko <55813967+fekoneko@users.noreply.github.com>
2026-01-22 15:42:03 +0300

    `docker-prune.sh`

---
+++ Scripts/docker-prune.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+read -rp "Asre you sure you want to nuke all docker data? [y/N] " ok
+case $ok in y|Y);; *) exit 0;; esac
+
+mapfile -rd ' ' containers < <(docker ps -a -q)
+[[ ${#containers[@]} != 0 ]] && docker kill "${containers[@]}"
+
+yes | docker system prune -a
+yes | docker volume prune -a
